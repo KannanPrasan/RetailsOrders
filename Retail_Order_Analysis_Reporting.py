04cccf90d296d3ef5328a576b357f356023e702d
KannanPrasan <Prasanislearning@gmail.com>
2024-12-24 17:57:36 +0530

    Update Retail_Order_Analysis_Reporting.py

--- Retail_Order_Analysis_Reporting.py
+++ Retail_Order_Analysis_Reporting.py
@@ -17,8 +17,19 @@ QUERIES = {
         , "8. Determine the average discount percentage given per region" : "SELECT REGION ,AVG(DISCOUNT_PERCENT) AS AVERAGE_DP FROM dfto_order2 E INNER JOIN dfto_order D ON (D.ORDER_ID = E.ORDER_ID) GROUP BY REGION ORDER BY 2 DESC ;"
         , "9. Find the product category with the highest total profit" : "SELECT CATEGORY ,SUM(SALE_PRICE-COST_PRICE) AS TOTAL_PROFIT FROM DFTO_ORDER2 GROUP BY CATEGORY LIMIT 1;"
         , "10.Calculate the total revenue generated per year": "SELECT SUBSTRING(ORDER_DATE, 1,4) ,sum(sale_price) AS AVERAGE_DP FROM dfto_order2 E INNER JOIN dfto_order D ON (D.ORDER_ID = E.ORDER_ID) GROUP BY SUBSTRING(ORDER_DATE, 1,4) ORDER BY 2 DESC ;"
+        ,"11 state with most products sold" : "SELECT State	,SUM(Quantity) AS Most_Quantity FROM dfto_order2 E INNER JOIN dfto_order D ON (D.ORDER_ID = E.ORDER_ID) GROUP BY State ORDER BY Most_Quantity DESC LIMIT 1;"
+        ,"12 Show me the top 10 products Ranked based on revenue" : "SELECT PRODUCT_ID,  SUM(sale_price), RANK() OVER (ORDER BY SUM(sale_price) DESC) AS Revenue_rank  FROM dfto_order2  GROUP BY      Product_Id;"
+        ,"13 Show me the number of products per Sub Category" : "SELECT SUB_CATEGORY, COUNT(DISTINCT PRODUCT_ID) FROM DFTO_ORDER2 GROUP BY SUB_CATEGORY ;"
+        ,"14 Which city has most discounted products" : "SELECT CITY 	,ROUND(SUM(DISCOUNTED_AMOUNT)) AS Most_Discount FROM dfto_order2 E INNER JOIN dfto_order D ON (D.ORDER_ID = E.ORDER_ID) GROUP BY CITY ORDER BY Most_Discount DESC LIMIT 1; "
+        ,"15 show me the least performing PRODUCTS who had no sales at all " : "SELECT PRODUCT_ID FROM dfto_order2 GROUP BY PRODUCT_ID HAVING SUM(SALE_PRICE)=0;"
+        ,"16 What is the sales in last 1 month" : "SELECT      D.Order_Date, D.ORDER_ID, STATE, CITY, CATEGORY, SUB_CATEGORY, PRODUCT_ID, SALE_PRICE FROM      dfto_order D     INNER JOIN dfto_order2 O ON D.ORDER_ID = O.ORDER_ID WHERE      Order_Date >= DATE_SUB((select max(Order_Date) from dfto_order), INTERVAL 1 MONTH)     ORDER BY 1 DESC;"
+        ,"17 Which month has given most revenue" : "SELECT      MONTHNAME(Order_Date) AS month,     SUM(Sale_Price) AS total_revenue FROM      dfto_order D     INNER JOIN dfto_order2 O ON D.ORDER_ID = O.ORDER_ID GROUP BY   MONTHNAME(Order_Date) ORDER BY      total_revenue DESC LIMIT 1;"
+        ,"18 Which state sold most number of phones" : "SELECT      State,     SUM(Quantity) AS Total_Quantity FROM      dfto_order D     INNER JOIN dfto_order2 O ON D.ORDER_ID = O.ORDER_ID WHERE      Sub_Category = 'Phones' GROUP BY   State ORDER BY      Total_Quantity DESC LIMIT 1;"
+        ,"19 What is the most sold product in Georgia" : "SELECT      Category, Sub_Category,Product_Id,     SUM(Quantity) AS total_quantity_sold FROM      dfto_order D     INNER JOIN dfto_order2 O ON D.ORDER_ID = O.ORDER_ID WHERE      State = 'Georgia' GROUP BY   Category, Sub_Category,Product_Id ORDER BY      total_quantity_sold DESC LIMIT 1;"
+        ,"20 Which State has used more Same Day delivery" : "SELECT      State,     Count(d.Order_ID) AS Total_Orders FROM      dfto_order d WHERE      Ship_Mode = 'Same Day' GROUP BY      State ORDER BY      Total_Orders DESC LIMIT 1;"
 
 }
+
 
 # Streamlit App
 st.title("Retail Orders: Requests provided from GUVI")
